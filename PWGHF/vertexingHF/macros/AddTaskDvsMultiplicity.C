5d74b8c1d414bef4a44d868ca18256e3e4c538a0
Yoshini Bailung <yoshini.bailung@cern.ch>
2020-09-21 14:21:37 +0530

    AddTaskDvsMultiplicity.C

--- PWGHF/vertexingHF/macros/AddTaskDvsMultiplicity.C
+++ PWGHF/vertexingHF/macros/AddTaskDvsMultiplicity.C
@@ -1,12 +1,12 @@
 AliAnalysisTaskSEDvsMultiplicity *AddTaskDvsMultiplicity(Int_t system=0,
 							 Bool_t readMC=kFALSE,
 							 Int_t MCOption=0,
-							 Int_t pdgMeson=421,
-							 TString finDirname="D0_2016",
-							 TString filename="D0toKpiCutsNewCutfinerPtBins.root",
-							 TString finAnObjname="D0toKpiCuts",
+							 Int_t pdgMeson=4122,
+							 TString finDirname="Lc2pK0S",
+							 TString filename="",
+							 TString finAnObjname="LctoV0AnalysisCuts",
 							 TString estimatorFilename="",
-							 Double_t refMult=12.25,
+							 Double_t refMult=9.26,
 							 Bool_t subtractDau=kFALSE,
 							 Int_t NchWeight=0,
 							 Int_t recoEstimator = AliAnalysisTaskSEDvsMultiplicity::kNtrk10,
@@ -33,8 +33,6 @@ AliAnalysisTaskSEDvsMultiplicity *AddTaskDvsMultiplicity(Int_t system=0,
     stdcuts=kTRUE;
   } else {
     filecuts=TFile::Open(filename.Data());
-    cout<<filename.Data()<<endl;
-    cout<<finAnObjname.Data()<<endl;
     if(!filecuts ||(filecuts&& !filecuts->IsOpen())){
       Printf("FATAL: Input file not found : check your cut object");
     }
@@ -54,14 +52,12 @@ AliAnalysisTaskSEDvsMultiplicity *AddTaskDvsMultiplicity(Int_t system=0,
     else analysiscuts = (AliRDHFCutsDplustoKpipi*)filecuts->Get(finAnObjname);
     Name="Dplus";
   }else if(pdgMeson==421){
-    cout<<"pdg meson Working"<<endl;
     if(stdcuts) {
       analysiscuts = new AliRDHFCutsD0toKpi();
       if (system == 0) analysiscuts->SetStandardCutsPP2010();
       else analysiscuts->SetStandardCutsPbPb2011();
     }
     else analysiscuts = (AliRDHFCutsD0toKpi*)filecuts->Get(finAnObjname);
-    cout<<analysiscuts<<endl;
     Name="D0";
   }else if(pdgMeson==413){
     if(stdcuts) {
@@ -79,7 +75,7 @@ AliAnalysisTaskSEDvsMultiplicity *AddTaskDvsMultiplicity(Int_t system=0,
     }
     else analysiscuts = (AliRDHFCutsDstoKKpi*)filecuts->Get(finAnObjname);
     Name="Ds";
-  }else if(pdgMeson==4122){
+  }else if(pdgMeson==4122 && isLcV0){
     if(stdcuts) {
       analysiscuts = new AliRDHFCutsLctoV0();
       if (system == 0) analysiscuts->SetStandardCutsPP2010();
@@ -87,8 +83,16 @@ AliAnalysisTaskSEDvsMultiplicity *AddTaskDvsMultiplicity(Int_t system=0,
     }
     else analysiscuts = (AliRDHFCutsLctoV0*)filecuts->Get(finAnObjname);
     Name="Lc2pK0S";
+  }else if(pdgMeson==4122 && !isLcV0){
+    if(stdcuts) {
+      analysiscuts = new AliRDHFCutsLctopKpi();
+      if (system == 0) analysiscuts->SetStandardCutsPP2010();
+      else analysiscuts->SetStandardCutsPbPb2011();
+    }
+    else analysiscuts = (AliRDHFCutsLctopKpi*)filecuts->Get(finAnObjname);
+    Name="Lc2pKpi";
   }
-    
+
   AliAnalysisTaskSEDvsMultiplicity *dMultTask = new AliAnalysisTaskSEDvsMultiplicity("dMultAnalysis",pdgMeson,analysiscuts,isPPbData);
   dMultTask->SetReadMC(readMC);
   dMultTask->SetDebugLevel(0);
@@ -317,5 +321,4 @@ const Char_t* periodNames[14]={"LHC18b","LHC18d","LHC18e","LHC18f", "LHC18g","LH
   mgr->ConnectOutput(dMultTask,4,coutputProf);
 
   return dMultTask;
-  
 }
